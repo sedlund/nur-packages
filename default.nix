6baa46f95c893d7cd48e94f6d3f07c857d92d2ba
Scott Edlund <sedlund@users.noreply.github.com>
2026-03-19 12:28:11 +0800

    feat: add ghoten package and replace example package

--- default.nix
+++ default.nix
@@ -6,7 +6,9 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 {
   # The `lib`, `modules`, and `overlays` names are special
@@ -14,7 +16,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  ghoten = pkgs.callPackage ./pkgs/ghoten { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
